afc5430b9d2e197f4b60d5900075a12eeab88efa
Carlos Sanchez <carlos.sanchez@nextthought.com>
2014-07-13 12:36:13 +0000

    update references
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.analytics/trunk@43057 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/app/analytics/views.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+.. $Id$
+"""
+from __future__ import print_function, unicode_literals, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+logger = __import__('logging').getLogger(__name__)
+
+from zope import interface
+from zope.location.interfaces import IContained
+from zope.container import contained as zcontained
+from zope.traversing.interfaces import IPathAdapter
+
+@interface.implementer(IPathAdapter, IContained)
+class AnalyticsPathAdapter(zcontained.Contained):
+
+	__name__ = 'analytics'
+
+	def __init__(self, context, request):
+		self.context = context
+		self.request = request
+		self.__parent__ = context
