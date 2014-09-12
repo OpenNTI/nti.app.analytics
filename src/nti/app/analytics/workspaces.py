db6ed6a9c983001c74f01ef9e493080744a5823b
Carlos Sanchez <carlos.sanchez@nextthought.com>
2014-09-12 19:49:02 +0000

    clean up
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.analytics/trunk@49294 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/analytics/workspaces.py
+++ src/nti/app/analytics/workspaces.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-Implementation of an Atom/OData workspace and collection
-for badges.
+Implementation of an Atom/OData workspace and collection for analytics.
 
 .. $Id$
 """
+
 from __future__ import print_function, unicode_literals, absolute_import, division
 __docformat__ = "restructuredtext en"
 
@@ -14,31 +14,32 @@ logger = __import__('logging').getLogger(__name__)
 from zope import interface
 from zope import component
 from zope.container import contained
-from zope.location import interfaces as loc_interfaces
+from zope.location.interfaces import ILocation
 
-import nti.appserver.interfaces as app_interfaces
+from nti.appserver.interfaces import IWorkspace
+from nti.appserver.interfaces import IUserService
 
-from nti.dataserver import links
-from nti.dataserver import interfaces as nti_interfaces
+from nti.dataserver.links import Link
+from nti.dataserver.interfaces import IDataserverFolder
 
 from nti.utils.property import alias
 
 from . import ANALYTICS
-from . import ANALYTICS_TITLE
 from . import BATCH_EVENTS
+from . import ANALYTICS_TITLE
 from . import ANALYTICS_SESSION
 
-from nti.app.analytics.interfaces import IAnalyticsWorkspace
+from .interfaces import IAnalyticsWorkspace
 
-@interface.implementer(app_interfaces.IWorkspace)
-@component.adapter(app_interfaces.IUserService)
-def AnalyticsWorkspace( user_service ):
-	analytics_ws = _AnalyticsWorkspace( parent=user_service.__parent__ )
+@interface.implementer(IWorkspace)
+@component.adapter(IUserService)
+def AnalyticsWorkspace(user_service):
+	analytics_ws = _AnalyticsWorkspace(parent=user_service.__parent__)
 	assert analytics_ws.__parent__
 	return analytics_ws
 
 @interface.implementer(IAnalyticsWorkspace)
-@component.adapter(nti_interfaces.IDataserverFolder)
+@component.adapter(IDataserverFolder)
 class _AnalyticsWorkspace(contained.Contained):
 	"""
 	A workspace that currently, does not have any collections, but
@@ -59,14 +60,12 @@ class _AnalyticsWorkspace(contained.Contained):
 
 	@property
 	def links(self):
-		link_names = [BATCH_EVENTS, ANALYTICS_SESSION]
-
 		result = []
+		link_names = [BATCH_EVENTS, ANALYTICS_SESSION]
 		for name in link_names:
-			#ds2/analytics/@@batch_events
-			link = links.Link(ANALYTICS, rel=name, elements=(name,))
+			link = Link(ANALYTICS, rel=name, elements=(name,))
 			link.__name__ = link.target
 			link.__parent__ = self.__parent__
-			interface.alsoProvides(link, loc_interfaces.ILocation)
+			interface.alsoProvides(link, ILocation)
 			result.append(link)
 		return result
