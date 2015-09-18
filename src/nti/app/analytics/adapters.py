ddcd2933d7720816223f1648b8fa1dedcadfb655
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-09-18 00:55:49 +0000

    formatting
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.analytics/trunk@73242 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/analytics/adapters.py
+++ src/nti/app/analytics/adapters.py
@@ -5,28 +5,29 @@ Adapters for application-level events.
 
 .. $Id$
 """
+
 from __future__ import print_function, unicode_literals, absolute_import, division
 __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
 
 from datetime import datetime
-
 from six import integer_types
 
-from zope import interface
 from zope import component
+from zope import interface
 
 from nti.app.products.courseware.interfaces import IViewStats
 from nti.app.products.courseware.interfaces import IVideoUsageStats
-from nti.dataserver.contenttypes.forums.interfaces import ITopic
 
 from nti.assessment.interfaces import IQAssignment
 from nti.assessment.interfaces import IQuestionSet
 
+from nti.contenttypes.courses.interfaces import ICourseInstance
+
 from nti.dataserver.interfaces import INote
 from nti.dataserver.interfaces import IUser
-from nti.contenttypes.courses.interfaces import ICourseInstance
+from nti.dataserver.contenttypes.forums.interfaces import ITopic
 
 from nti.analytics import has_analytics
 from nti.analytics.interfaces import IProgress
@@ -43,9 +44,9 @@ from nti.analytics.progress import DefaultProgress
 
 from .video_usage_stats import VideoUsageReport
 
-@interface.implementer( IProgress )
-@component.adapter( IUser, IQAssignment )
-def _assignment_progress_for_user( user, assignment ):
+@interface.implementer(IProgress)
+@component.adapter(IUser, IQAssignment)
+def _assignment_progress_for_user(user, assignment):
 	"""
 	Given an assignment and a user, we
 	attempt to determine the amount of progress the user
@@ -54,18 +55,18 @@ def _assignment_progress_for_user( user, assignment ):
 	"""
 	# In local tests, about 100 objects are decorated in about 1s;
 	# this is in UCOL with a lot of assignments but few assessments.
-	assignment_id = getattr( assignment, 'ntiid', None )
-	assignment_records = get_assignment_for_user( user, assignment_id )
+	assignment_id = getattr(assignment, 'ntiid', None)
+	assignment_records = get_assignment_for_user(user, assignment_id)
 	result = None
 	if assignment_records:
 		# Simplistic implementation
-		last_mod = max( (x.timestamp for x in assignment_records) )
-		result = DefaultProgress( assignment_id, 1, 1, True, last_modified=last_mod )
+		last_mod = max((x.timestamp for x in assignment_records))
+		result = DefaultProgress(assignment_id, 1, 1, True, last_modified=last_mod)
 	return result
 
-@interface.implementer( IProgress )
-@component.adapter( IUser, IQuestionSet )
-def _assessment_progress_for_user( user, assessment ):
+@interface.implementer(IProgress)
+@component.adapter(IUser, IQuestionSet)
+def _assessment_progress_for_user(user, assessment):
 	"""
 	Given a generic assessment and a user, we
 	attempt to determine the amount of progress the user
@@ -78,94 +79,92 @@ def _assessment_progress_for_user( user, assessment ):
 	# If we have a cache, the cost is trivial.
 	# Or we only care about possible self-assessments here; if we have a record
 	# great, else we do not return anything.
-	assessment_id = getattr( assessment, 'ntiid', None )
-	assessment_records = get_self_assessments_for_user_and_id( user, assessment_id )
+	assessment_id = getattr(assessment, 'ntiid', None)
+	assessment_records = get_self_assessments_for_user_and_id(user, assessment_id)
 	result = None
 	if assessment_records:
 		# Simplistic implementation
-		last_mod = max( (x.timestamp for x in assessment_records) )
-		result = DefaultProgress( assessment_id, 1, 1, True, last_modified=last_mod )
+		last_mod = max((x.timestamp for x in assessment_records))
+		result = DefaultProgress(assessment_id, 1, 1, True, last_modified=last_mod)
 	return result
 
-class _ViewStats( object ):
+class _ViewStats(object):
 
-	def __init__(self, view_count, new_reply_count_for_user=0 ):
+	def __init__(self, view_count, new_reply_count_for_user=0):
 		self.view_count = view_count
 		self.new_reply_count_for_user = new_reply_count_for_user
 
-def _get_stats( records, replies=None, user=None ):
-	count = len( records ) if records else 0
-	reply_count = len( replies ) if replies else 0
+def _get_stats(records, replies=None, user=None):
+	count = len(records) if records else 0
+	reply_count = len(replies) if replies else 0
 	if user is None:
 		# We just want the count
-		result = _ViewStats( count, 0 )
+		result = _ViewStats(count, 0)
 	elif not records:
 		# User but no views
-		result = _ViewStats( count, reply_count )
+		result = _ViewStats(count, reply_count)
 	else:
 		# Ok, find out how many replies since our user viewed the object.
 		user_view_times = [x.timestamp for x in records if x.user == user and x.timestamp]
 
 		if user_view_times:
-			user_last_viewed = max( user_view_times ) if user_view_times else 0
+			user_last_viewed = max(user_view_times) if user_view_times else 0
 			new_reply_count_for_user = 0
 			for reply in replies:
 				reply_created_time = reply.createdTime
-				if isinstance( reply_created_time, (integer_types, float) ):
-					reply_created_time = datetime.utcfromtimestamp( reply_created_time )
+				if isinstance(reply_created_time, (integer_types, float)):
+					reply_created_time = datetime.utcfromtimestamp(reply_created_time)
 				if reply_created_time and reply_created_time > user_last_viewed:
 					new_reply_count_for_user += 1
-			result = _ViewStats( count, new_reply_count_for_user )
+			result = _ViewStats(count, new_reply_count_for_user)
 		else:
-			result = _ViewStats( count, reply_count )
+			result = _ViewStats(count, reply_count)
 	return result
 
-@interface.implementer( IViewStats )
-@component.adapter( ITopic )
-def _topic_view_stats( topic ):
+@interface.implementer(IViewStats)
+@component.adapter(ITopic)
+def _topic_view_stats(topic):
 	result = None
 	if has_analytics():
-		records = get_topic_views( topic=topic )
-		result = _get_stats( records )
+		records = get_topic_views(topic=topic)
+		result = _get_stats(records)
 	return result
 
-@interface.implementer( IViewStats )
-@component.adapter( ITopic, IUser )
-def _topic_view_stats_for_user( topic, user ):
+@interface.implementer(IViewStats)
+@component.adapter(ITopic, IUser)
+def _topic_view_stats_for_user(topic, user):
 	result = None
 	if has_analytics():
-		records = get_topic_views( topic=topic )
+		records = get_topic_views(topic=topic)
 		replies = topic.values()
-		result = _get_stats( records, replies, user )
+		result = _get_stats(records, replies, user)
 	return result
 
-@interface.implementer( IViewStats )
-@component.adapter( INote )
-def _note_view_stats( note ):
+@interface.implementer(IViewStats)
+@component.adapter(INote)
+def _note_view_stats(note):
 	result = None
 	if has_analytics():
-		records = get_note_views( note=note )
-		result = _get_stats( records )
+		records = get_note_views(note=note)
+		result = _get_stats(records)
 	return result
 
-@interface.implementer( IViewStats )
-@component.adapter( INote, IUser )
-def _note_view_stats_for_user( note, user ):
+@interface.implementer(IViewStats)
+@component.adapter(INote, IUser)
+def _note_view_stats_for_user(note, user):
 	result = None
 	if has_analytics():
-		records = get_note_views( note=note )
+		records = get_note_views(note=note)
 		replies = note.referents
-		result = _get_stats( records, replies, user )
+		result = _get_stats(records, replies, user)
 	return result
 
-@interface.implementer( IVideoUsageStats )
-@component.adapter( ICourseInstance )
-def _video_usage_stats( course ):
+@interface.implementer(IVideoUsageStats)
+@component.adapter(ICourseInstance)
+def _video_usage_stats(course):
 	result = None
 	if has_analytics():
-		video_events = get_video_views( course=course )
+		video_events = get_video_views(course=course)
 		report = VideoUsageReport()
-		result = report.get_video_usage_stats( video_events, course )
+		result = report.get_video_usage_stats(video_events, course)
 	return result
-	
-
