e407120e9fc74cc60689e0d63b30ec41bc0c39d9
Zachary Roux <zachary.roux@nextthought.com>
2015-10-29 22:17:50 +0000

    Changed test case to a city name with a special character to help test unicode compatibility.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.analytics/trunk@75791 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/analytics/tests/test_views.py
+++ src/nti/app/analytics/tests/test_views.py
@@ -803,23 +803,23 @@ class TestUserLocationView( _AbstractTestViews ):
 		# Create test locations
 		location1 = Location(latitude='10.0000',
 							longitude='10.0000',
-							city='Atlantis',
-							state='Solid',
-							country='Knowhere'
+							city='Zürich', #Note the ü. This is to test that unicode works. 
+							state='',
+							country='Switzerland'
 							)
 
 		location2 = Location(latitude='11.0000',
 							longitude='11.0000',
-							city='The Forbidden City',
-							state='Liquid',
-							country='United States'
+							city = 'The Forbidden City',
+							state = 'Liquid',
+							country = 'United States'
 							)
 
 		location3 = Location(latitude='12.0000',
 							longitude='12.0000',
-							city='Running out of city names',
-							state='Unknown',
-							country='Great Wilderness'
+							city = 'Running out of city names',
+							state = 'Unknown',
+							country = 'Great Wilderness'
 							)
 
 		# Add test locations
@@ -979,7 +979,7 @@ class TestUserLocationView( _AbstractTestViews ):
 		location_json_result = location_view()[0]
 		json_result = [location_json_result['latitude'],
 					location_json_result['longitude'],
-					location_json_result['label'].encode('ascii','ignore')]
+					location_json_result['label']]
 
 		# The html output should contain the same location data as in the json result.
 		assert_that( str(result.html), contains_string( str( json_result ) ) )
@@ -999,7 +999,7 @@ class TestUserLocationView( _AbstractTestViews ):
 		for view in location_json_result:
 			json_result.append([view['latitude'],
 								view['longitude'],
-								view['label'].encode('ascii','ignore')])
+								view['label']])
 		assert_that( str(result.html), contains_string( str( json_result ) ) )
 
 		# Add another user in the first location
@@ -1020,7 +1020,7 @@ class TestUserLocationView( _AbstractTestViews ):
 		for view in location_json_result:
 			json_result.append([view['latitude'],
 								view['longitude'],
-								view['label'].encode('ascii','ignore')])
+								view['label']])
 		assert_that( str(result.html), contains_string( str( json_result ) ) )
 
 		# The second user has another ip address in a location not shared by the first
@@ -1040,5 +1040,5 @@ class TestUserLocationView( _AbstractTestViews ):
 		for view in location_json_result:
 			json_result.append([view['latitude'],
 								view['longitude'],
-								view['label'].encode('ascii','ignore')])
+								view['label']])
 		assert_that( str(result.html), contains_string( str( json_result ) ) )
