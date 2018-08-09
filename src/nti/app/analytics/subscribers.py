504aba18a29fbe38981a8ca484da1a5142703e30
Carlos Sanchez <carlos.sanchez@nextthought.com>
2018-08-09 17:46:10 -0500

    pylint; pep8

--- src/nti/app/analytics/subscribers.py
+++ src/nti/app/analytics/subscribers.py
@@ -9,26 +9,26 @@ from __future__ import absolute_import
 
 from zope import component
 
-from nti.analytics.sessions import handle_end_session
+from nti.app.analytics.utils import get_session_id_from_request
 
 from nti.appserver.interfaces import IUserLogoutEvent
-from nti.app.analytics.utils import get_session_id_from_request
+
+from nti.analytics.sessions import handle_end_session
 
 logger = __import__('logging').getLogger(__name__)
 
 
-@component.adapter( IUserLogoutEvent )
+@component.adapter(IUserLogoutEvent)
 def _user_logout_event(event):
-	"""
-	When a user logs out, terminate any sessions tied to the request.
-	We only do this for cookies (e.g. webapp/mobile) since other clients
-	manage their own sessions explicitly.
-	"""
-	session_id = get_session_id_from_request(event.request)
-
-	if session_id is not None:
-		user = event.user
-		username = getattr(user, 'username', None)
-		if username is None:
-			username = getattr(event.request,'remote_user', None)
-		handle_end_session(username, session_id)
+    """
+    When a user logs out, terminate any sessions tied to the request.
+    We only do this for cookies (e.g. webapp/mobile) since other clients
+    manage their own sessions explicitly.
+    """
+    session_id = get_session_id_from_request(event.request)
+    if session_id is not None:
+        user = event.user
+        username = getattr(user, 'username', None)
+        if username is None:
+            username = getattr(event.request, 'remote_user', None)
+        handle_end_session(username, session_id)
